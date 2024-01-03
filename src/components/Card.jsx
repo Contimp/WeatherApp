2dadbb1b89d1e7670d6a8655cbda2cd4f7cc3d8b
Contimp <contimariapamela@gmail.com>
2024-01-03 11:59:07 -0300

    Css Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import style from  './Card.module.css'
+import style from "./Card.module.css";
 
-export default function Card (props){
+export default function Card(props) {
   return (
-    <div className={style.container}>
+    <div className={style.card}>
       <button className={style.button} onClick={props.onClose}>
         X
       </button>
       <h4 className={style.title}>{props.name}</h4>
       <section className={style.section}>
-        <p>Min</p>
-        <p>{props.min}</p>
-      </section>
-      <section className={style.section}>
-        <p>Max</p>
-        <p>{props.max}</p>
-      </section>
+        <div className={style.temp}>
+          <p>Min</p>
+          <p>{props.min}</p>
+        </div>
+        <div className={style.temp}>
+          <p>Max</p>
+          <p>{props.max}</p>
+        </div>
+        <div>
       <img
         className={style.img}
         src={`http://openweathermap.org/img/wn/${props.img}@2x.png`}
         alt="weather image"
       />
+        </div>
+      </section>
     </div>
   );
 }
