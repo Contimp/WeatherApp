b9457bb94b2d662a9150a9878a3987ef15ca2e63
Contimp <contimariapamela@gmail.com>
2024-01-07 11:30:09 -0300

    Css SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import style from './SearchBar.module.css'
-
+import style from './SearchBar.module.css';
+import { FaSearch } from "react-icons/fa";
 export default function SearchBar(props){
   return (
     <div>
       <input className={style.input} placeholder="Add City..." />
-      <button className={style.button} onClick = {()=> props.onSearch('Searching...')}>Search</button>
+      <button
+        className={style.button}
+        onClick={() => props.onSearch("Searching...")}
+      >
+        <FaSearch />
+      </button>
     </div>
   );
 }
